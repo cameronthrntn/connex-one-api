0b0c1f879a0878a31f77528a7493cbefc627a1ac
cameronthrntn <contact@cameronthornton.dev>
2023-08-03 16:05:44 +0100

    adds prometheus metrics

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import promMid from "express-prometheus-middleware";
 import middleware from "./middleware";
 
 dotenv.config();
@@ -7,7 +8,18 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/time", middleware, (req: Request, res: Response) => {
+app.use(middleware);
+
+app.use(
+  promMid({
+    collectGCMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+  })
+);
+
+app.get("/time", (req: Request, res: Response) => {
   res.send({
     epoch: Date.now(),
   });
