c38686967a88558fe6ecc60a2f5e26df7cf48fdb
cameronthrntn <contact@cameronthornton.dev>
2023-08-17 22:13:08 +0100

    enables ts-ignore

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ app.use(
     requestDurationBuckets: [0.1, 0.5, 1, 1.5],
     requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
     responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
-  })
+  }),
 );
 
 app.get("/time", (req: Request, res: Response) => {
